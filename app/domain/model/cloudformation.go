08cd3064bb47a8bad086c4bdaee49025280652b3
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-01-13 22:29:35 +0900

    Add cloudformation service method

--- app/domain/model/cloudformation.go
+++ app/domain/model/cloudformation.go
@@ -5,6 +5,9 @@ import "time"
 const (
 	// CloudFormationRetryMaxAttempts is the maximum number of retries for CloudFormation.
 	CloudFormationRetryMaxAttempts int = 2
+	// CloudFormationWaitNanoSecTime is the time to wait for CloudFormation.
+	// It is 1 hour.
+	CloudFormationWaitNanoSecTime = time.Duration(6000000000000000)
 )
 
 // StackStatus is the status of a CloudFormation stack.
@@ -83,4 +86,175 @@ type Stack struct {
 	StackStatusReason *string
 	// TemplateDescription is the template description of the template used to create the stack.
 	TemplateDescription *string
+}
+
+// ResourceStatus is the status of a CloudFormation stack resource.
+type ResourceStatus string
+
+const (
+	// ResourceStatusCreateInProgress is the resource is being created.
+	ResourceStatusCreateInProgress ResourceStatus = "CREATE_IN_PROGRESS"
+	// ResourceStatusCreateFailed is the resource creation failed.
+	ResourceStatusCreateFailed ResourceStatus = "CREATE_FAILED"
+	// ResourceStatusCreateComplete is the resource has been created.
+	ResourceStatusCreateComplete ResourceStatus = "CREATE_COMPLETE"
+	// ResourceStatusDeleteInProgress is the resource is being deleted.
+	ResourceStatusDeleteInProgress ResourceStatus = "DELETE_IN_PROGRESS"
+	// ResourceStatusDeleteFailed is the resource deletion failed.
+	ResourceStatusDeleteFailed ResourceStatus = "DELETE_FAILED"
+	// ResourceStatusDeleteComplete is the resource has been deleted.
+	ResourceStatusDeleteComplete ResourceStatus = "DELETE_COMPLETE"
+	// ResourceStatusDeleteSkipped is the resource was not successfully deleted. It might still be
+	ResourceStatusDeleteSkipped ResourceStatus = "DELETE_SKIPPED"
+	// ResourceStatusUpdateInProgress is the resource is being updated.
+	ResourceStatusUpdateInProgress ResourceStatus = "UPDATE_IN_PROGRESS"
+	// ResourceStatusUpdateFailed is the resource update failed.
+	ResourceStatusUpdateFailed ResourceStatus = "UPDATE_FAILED"
+	// ResourceStatusUpdateComplete is the resource has been updated.
+	ResourceStatusUpdateComplete ResourceStatus = "UPDATE_COMPLETE"
+	// ResourceStatusImportFailed is the resource import failed.
+	ResourceStatusImportFailed ResourceStatus = "IMPORT_FAILED"
+	// ResourceStatusImportComplete is the resource has been imported.
+	ResourceStatusImportComplete ResourceStatus = "IMPORT_COMPLETE"
+	// ResourceStatusImportInProgress is the resource is being imported into a stack.
+	ResourceStatusImportInProgress ResourceStatus = "IMPORT_IN_PROGRESS"
+	// ResourceStatusImportRollbackInProgress is the resource is being rolled back to its previous
+	ResourceStatusImportRollbackInProgress ResourceStatus = "IMPORT_ROLLBACK_IN_PROGRESS"
+	// ResourceStatusImportRollbackFailed is the resource import failed and the resource is
+	ResourceStatusImportRollbackFailed ResourceStatus = "IMPORT_ROLLBACK_FAILED"
+	// ResourceStatusImportRollbackComplete is the resource was rolled back to its previous
+	ResourceStatusImportRollbackComplete ResourceStatus = "IMPORT_ROLLBACK_COMPLETE"
+	// ResourceStatusUpdateRollbackInProgress is the resource is being rolled back as part of a
+	ResourceStatusUpdateRollbackInProgress ResourceStatus = "UPDATE_ROLLBACK_IN_PROGRESS"
+	// ResourceStatusUpdateRollbackComplete is the resource was rolled back to its previous
+	ResourceStatusUpdateRollbackComplete ResourceStatus = "UPDATE_ROLLBACK_COMPLETE"
+	// ResourceStatusUpdateRollbackFailed is the resource update failed and the resource is being rolled back to its previous configuration.
+	ResourceStatusUpdateRollbackFailed ResourceStatus = "UPDATE_ROLLBACK_FAILED"
+	// ResourceStatusRollbackInProgress is the resource is being rolled back.
+	ResourceStatusRollbackInProgress ResourceStatus = "ROLLBACK_IN_PROGRESS"
+	// ResourceStatusRollbackComplete is the resource was rolled back.
+	ResourceStatusRollbackComplete ResourceStatus = "ROLLBACK_COMPLETE"
+	// ResourceStatusRollbackFailed is the resource rollback failed.
+	ResourceStatusRollbackFailed ResourceStatus = "ROLLBACK_FAILED"
+)
+
+// Values returns all known values for ResourceStatus. Note that this can be
+// expanded in the future, and so it is only as up to date as the client. The
+// ordering of this slice is not guaranteed to be stable across updates.
+func (ResourceStatus) Values() []ResourceStatus {
+	return []ResourceStatus{
+		ResourceStatusCreateInProgress,
+		ResourceStatusCreateFailed,
+		ResourceStatusCreateComplete,
+		ResourceStatusDeleteInProgress,
+		ResourceStatusDeleteFailed,
+		ResourceStatusDeleteComplete,
+		ResourceStatusDeleteSkipped,
+		ResourceStatusUpdateInProgress,
+		ResourceStatusUpdateFailed,
+		ResourceStatusUpdateComplete,
+		ResourceStatusImportFailed,
+		ResourceStatusImportComplete,
+		ResourceStatusImportInProgress,
+		ResourceStatusImportRollbackInProgress,
+		ResourceStatusImportRollbackFailed,
+		ResourceStatusImportRollbackComplete,
+		ResourceStatusUpdateRollbackInProgress,
+		ResourceStatusUpdateRollbackComplete,
+		ResourceStatusUpdateRollbackFailed,
+		ResourceStatusRollbackInProgress,
+		ResourceStatusRollbackComplete,
+		ResourceStatusRollbackFailed,
+	}
+}
+
+// StackResourceDriftStatus is status of the resource's actual configuration
+type StackResourceDriftStatus string
+
+const (
+	// StackResourceDriftStatusInSync is the resource's actual configuration matches its expected
+	StackResourceDriftStatusInSync StackResourceDriftStatus = "IN_SYNC"
+	// StackResourceDriftStatusModified is the resource differs from its expected configuration.
+	StackResourceDriftStatusModified StackResourceDriftStatus = "MODIFIED"
+	// StackResourceDriftStatusDeleted is the resource differs from its expected configuration in that it has been deleted.
+	StackResourceDriftStatusDeleted StackResourceDriftStatus = "DELETED"
+	// StackResourceDriftStatusNotChecked is CloudFormation hasn't checked if the resource differs from its expected configuration.
+	StackResourceDriftStatusNotChecked StackResourceDriftStatus = "NOT_CHECKED"
+)
+
+// Values returns all known values for StackResourceDriftStatus. Note that this
+// can be expanded in the future, and so it is only as up to date as the client.
+// The ordering of this slice is not guaranteed to be stable across updates.
+func (StackResourceDriftStatus) Values() []StackResourceDriftStatus {
+	return []StackResourceDriftStatus{
+		StackResourceDriftStatusInSync,
+		StackResourceDriftStatusModified,
+		StackResourceDriftStatusDeleted,
+		StackResourceDriftStatusNotChecked,
+	}
+}
+
+// StackResourceDriftInformationSummary is summarizes information about whether
+// the resource's actual configuration differs, or has drifted, from its expected configuration.
+type StackResourceDriftInformationSummary struct {
+	// StackResourceDriftStatus is status of the resource's actual
+	// configuration compared to its expected configuration.
+	StackResourceDriftStatus StackResourceDriftStatus
+	// LastCheckTimestamp is when CloudFormation last checked if the
+	// resource had drifted from its expected configuration.
+	LastCheckTimestamp *time.Time
+}
+
+// ModuleInfo is contains information about the module from which the resource
+// was created, if the resource was created from a module included in the stack
+// template. For more information about modules, see Using modules to encapsulate
+// and reuse resource configurations (https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/modules.html)
+// in the CloudFormation User Guide.
+type ModuleInfo struct {
+	// LogicalIDHierarchy is a concatenated list of the logical IDs of the module
+	// or modules containing the resource. Modules are listed starting with the
+	// inner-most nested module, and separated by / .
+	// In the following example, the resource was created from a module, moduleA,
+	// that's nested inside a parent module, moduleB . moduleA/moduleB For more
+	// information, see Referencing resources in a module
+	// (https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/modules.html#module-ref-resources)
+	// in the CloudFormation User Guide.
+	LogicalIDHierarchy *string
+	// TypeHierarchy is a concatenated list of the module type or types containing
+	// the resource. Module types are listed starting with the inner-most nested
+	// module, and separated by /.
+	// In the following example, the resource was created from a module of type
+	// AWS::First::Example::MODULE , that's nested inside a parent module of type
+	// AWS::Second::Example::MODULE .
+	// AWS::First::Example::MODULE/AWS::Second::Example::MODULE
+	TypeHierarchy *string
+}
+
+// StackResource is a CloudFormation stack resource. It is same as types.StackResourceSummary.
+type StackResource struct {
+	// LastUpdatedTimestamp is time the status was updated.
+	LastUpdatedTimestamp *time.Time
+	// LogicalResourceID is the logical name of the resource specified in the template.
+	LogicalResourceID *string
+	// ResourceStatus is current status of the resource.
+	ResourceStatus ResourceStatus
+	// ResourceType is type of resource. For more information, go to Amazon Web Services Resource
+	// Types Reference (https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-template-resource-type-ref.html)
+	// in the CloudFormation User Guide.
+	ResourceType *string
+	// DriftInformation is information about whether the resource's actual
+	// configuration differs, or has drifted, from its expected configuration,
+	// as defined in the stack template and any values specified as template
+	// parameters. For more information, see Detecting Unregulated Configuration
+	// Changes to Stacks and Resources
+	// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-stack-drift.html
+	DriftInformation *StackResourceDriftInformationSummary
+	// ModuleInfo is contains information about the module from which the resource was created, if
+	// the resource was created from a module included in the stack template.
+	ModuleInfo *ModuleInfo
+	// PhysicalResourceID is the name or unique identifier that corresponds to a
+	// physical instance ID of the resource.
+	PhysicalResourceID *string
+	// ResourceStatusReason is success/failure message associated with the resource.
+	ResourceStatusReason *string
 }
--- app/external/cloudformation.go
+++ app/external/cloudformation.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
@@ -20,35 +21,40 @@ func NewCloudFormationClient(cfg *model.AWSConfig) *cloudformation.Client {
 		})
 }
 
-// CloudFormationStackLister implements the CloudFormationStackLister interface.
-type CloudFormationStackLister struct {
+// NewCloudFormationStackDeleteCompleteWaiter returns a new CloudFormationStackDeleteCompleteWaiter.
+func NewCloudFormationStackDeleteCompleteWaiter(client *cloudformation.Client) *cloudformation.StackDeleteCompleteWaiter {
+	return cloudformation.NewStackDeleteCompleteWaiter(client)
+}
+
+// CFnStackLister implements the CFnStackLister interface.
+type CFnStackLister struct {
 	client *cloudformation.Client
 }
 
-// CloudFormationStackListerSet is a set of CloudFormationStackLister.
+// CFnStackListerSet is a set of CFnStackLister.
 //
 //nolint:gochecknoglobals
-var CloudFormationStackListerSet = wire.NewSet(
-	NewCloudFormationStackLister,
-	wire.Bind(new(service.CloudFormationStackLister), new(*CloudFormationStackLister)),
+var CFnStackListerSet = wire.NewSet(
+	NewCFnStackLister,
+	wire.Bind(new(service.CFnStackLister), new(*CFnStackLister)),
 )
 
-var _ service.CloudFormationStackLister = (*CloudFormationStackLister)(nil)
+var _ service.CFnStackLister = (*CFnStackLister)(nil)
 
-// NewCloudFormationStackLister returns a new CloudFormationStackLister.
-func NewCloudFormationStackLister(client *cloudformation.Client) *CloudFormationStackLister {
-	return &CloudFormationStackLister{client: client}
+// NewCFnStackLister returns a new CloudFormationStackLister.
+func NewCFnStackLister(client *cloudformation.Client) *CFnStackLister {
+	return &CFnStackLister{client: client}
 }
 
-// CloudFormationStackLister returns a list of CloudFormation stacks.
-func (l *CloudFormationStackLister) CloudFormationStackLister(ctx context.Context, input *service.CloudFormationStackListerInput) (*service.CloudFormationStackListerOutput, error) {
+// CFnStackLister returns a list of CloudFormation stacks.
+func (l *CFnStackLister) CFnStackLister(ctx context.Context, _ *service.CFnStackListerInput) (*service.CFnStackListerOutput, error) {
 	in := &cloudformation.ListStacksInput{}
 	stacks := make([]*model.Stack, 0, 100)
 
 	for {
 		select {
 		case <-ctx.Done():
-			return &service.CloudFormationStackListerOutput{
+			return &service.CFnStackListerOutput{
 				Stacks: stacks,
 			}, ctx.Err()
 		default:
@@ -84,7 +90,152 @@ func (l *CloudFormationStackLister) CloudFormationStackLister(ctx context.Contex
 		in.NextToken = out.NextToken
 	}
 
-	return &service.CloudFormationStackListerOutput{
+	return &service.CFnStackListerOutput{
 		Stacks: stacks,
 	}, nil
+}
+
+// CFnStackResourceLister implements the CFnStackResourceLister interface.
+type CFnStackResourceLister struct {
+	client *cloudformation.Client
+}
+
+// CFnStackResourceListerSet is a set of CFnStackResourceLister.
+//
+//nolint:gochecknoglobals
+var CFnStackResourceListerSet = wire.NewSet(
+	NewCFnStackResourceLister,
+	wire.Bind(new(service.CFnStackResourceLister), new(*CFnStackResourceLister)),
+)
+
+var _ service.CFnStackResourceLister = (*CFnStackResourceLister)(nil)
+
+// NewCFnStackResourceLister returns a new CloudFormationStackResourceLister.
+func NewCFnStackResourceLister(client *cloudformation.Client) *CFnStackResourceLister {
+	return &CFnStackResourceLister{client: client}
+}
+
+// CFnStackResourceLister returns a list of CloudFormation stack resources.
+func (l *CFnStackResourceLister) CFnStackResourceLister(ctx context.Context, input *service.CFnStackResourceListerInput) (*service.CFnStackResourceListerOutput, error) {
+	in := &cloudformation.ListStackResourcesInput{
+		StackName: aws.String(input.StackName),
+	}
+	resources := make([]*model.StackResource, 0, 100)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return &service.CFnStackResourceListerOutput{
+				Resources: resources,
+			}, ctx.Err()
+		default:
+		}
+
+		out, err := l.client.ListStackResources(ctx, in)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, resource := range out.StackResourceSummaries {
+			resources = append(resources, &model.StackResource{
+				LastUpdatedTimestamp: resource.LastUpdatedTimestamp,
+				LogicalResourceID:    resource.LogicalResourceId,
+				ResourceStatus:       model.ResourceStatus(resource.ResourceStatus),
+				ResourceType:         resource.ResourceType,
+				DriftInformation: &model.StackResourceDriftInformationSummary{
+					StackResourceDriftStatus: model.StackResourceDriftStatus(resource.DriftInformation.StackResourceDriftStatus),
+					LastCheckTimestamp:       resource.DriftInformation.LastCheckTimestamp,
+				},
+				PhysicalResourceID:   resource.PhysicalResourceId,
+				ResourceStatusReason: resource.ResourceStatusReason,
+			})
+		}
+		if out.NextToken == nil {
+			break
+		}
+		in.NextToken = out.NextToken
+	}
+
+	return &service.CFnStackResourceListerOutput{
+		Resources: resources,
+	}, nil
+}
+
+// CFnStackCreator implements the CFnStackCreator interface.
+type CFnStackCreator struct {
+	client *cloudformation.Client
+}
+
+// CFnStackCreatorSet is a set of CFnStackCreator.
+//
+//nolint:gochecknoglobals
+var CFnStackCreatorSet = wire.NewSet(
+	NewCFnStackCreator,
+	wire.Bind(new(service.CFnStackCreator), new(*CFnStackCreator)),
+)
+
+var _ service.CFnStackCreator = (*CFnStackCreator)(nil)
+
+// NewCFnStackCreator returns a new CloudFormationStackCreator.
+func NewCFnStackCreator(client *cloudformation.Client) *CFnStackCreator {
+	return &CFnStackCreator{client: client}
+}
+
+// CFnStackCreator creates a CloudFormation stack.
+func (c *CFnStackCreator) CFnStackCreator(ctx context.Context, input *service.CFnStackCreatorInput) (*service.CFnStackCreatorOutput, error) {
+	in := &cloudformation.CreateStackInput{
+		StackName:    aws.String(input.StackName),
+		TemplateBody: aws.String(input.TemplateBody),
+	}
+	out, err := c.client.CreateStack(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	return &service.CFnStackCreatorOutput{
+		StackID: *out.StackId,
+	}, nil
+}
+
+// CFnStackDeleter implements the CFnStackDeleter interface.
+type CFnStackDeleter struct {
+	client *cloudformation.Client
+	waiter *cloudformation.StackDeleteCompleteWaiter
+}
+
+// CFnStackDeleterSet is a set of CFnStackDeleter.
+//
+//nolint:gochecknoglobals
+var CFnStackDeleterSet = wire.NewSet(
+	NewCFnStackDeleter,
+	wire.Bind(new(service.CFnStackDeleter), new(*CFnStackDeleter)),
+)
+
+var _ service.CFnStackDeleter = (*CFnStackDeleter)(nil)
+
+// NewCFnStackDeleter returns a new CloudFormationStackDeleter.
+func NewCFnStackDeleter(client *cloudformation.Client, waiter *cloudformation.StackDeleteCompleteWaiter) *CFnStackDeleter {
+	return &CFnStackDeleter{
+		client: client,
+		waiter: waiter,
+	}
+}
+
+// CFnStackDeleter deletes a CloudFormation stack.
+func (d *CFnStackDeleter) CFnStackDeleter(ctx context.Context, input *service.CFnStackDeleterInput) (*service.CFnStackDeleterOutput, error) {
+	in := &cloudformation.DeleteStackInput{
+		StackName: aws.String(input.StackName),
+	}
+	_, err := d.client.DeleteStack(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = d.waiter.Wait(ctx, &cloudformation.DescribeStacksInput{
+		StackName: aws.String(input.StackName),
+	}, model.CloudFormationWaitNanoSecTime); err != nil && !strings.Contains(err.Error(), "waiter state transitioned to Failure") {
+		return nil, err
+	}
+
+	return &service.CFnStackDeleterOutput{}, nil
 }
