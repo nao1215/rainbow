bf2697e273591705b109758f1e2faa3839b9131e
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-01-16 23:23:02 +0900

    Add cloudformation template for creating s3

--- cloudformation/static-web-site-distribution/deploy.sh
+++ cloudformation/static-web-site-distribution/deploy.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 
 aws cloudformation create-stack \
-    --stack-name static-web-site-distribution \
-    --template-body template.yml \
-    --parameters parameters.json \
+    --endpoint-url "http://localhost:4566" \
+    --stack-name "static-web-site-distribution" \
+    --template-body "file://template.yml" \
+    --parameters "file://parameters.json" \
     --capabilities CAPABILITY_NAMED_IAM
